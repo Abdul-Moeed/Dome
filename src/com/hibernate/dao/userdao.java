5bf61631171daa2c54a3bb6a611ac27daac40056
Farjad Malik <m.farjad.malik@gmail.com>
2015-05-17 11:37:14 +0500

    Next

--- src/com/hibernate/dao/userdao.java
+++ src/com/hibernate/dao/userdao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.hibernate.util.users;
 
-public interface userdao {
+public interface userdao{
 	public List<users> getuser(int key);
 	public List<users> getuser(String email);
 	public List<users> getusernum(String phone_number);
